5badd0b8a025cbc53a169aea26a675a7970d9257
Greg McWhirter <greg@hallofkvasir.org>
2008-05-23 22:33:44 -0400

    Progress updates for team and personal stats generation

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -6,28 +6,28 @@ class StatisticsController < ApplicationController
   def index
     redirect_to :action => "standings"
   end
-  
+
   def standings
   	@brackets = Bracket.find(:all, :include => {:games => {:teams => :team_games}}).push nil
   	@allteams = Team.find(:all, :include => {:team_games => {:game => [:teams, :team_games]}})
 	@allteams.sort!{|a,b| sort_teams(a,b,nil)}
     	@max_round = Round.maximum('number')
-    	
+
     	@teams = {}
     	@brackets.each do |bracket|
 	    	@teams[bracket] = bracket.teams.sort{|a,b| sort_teams(a,b,bracket)} unless bracket.nil?
 	end
   end
-  
+
   def scoreboard
   	@rounds = Round.find(:all, :order => 'number, team_games.points desc', :include => {:games => [{:team_games => :team}, :room]})
   	teams = Team.find(:all, :order => 'name', :include => {:games => :round})
   	@byes = Hash.new
-  	
+
   	@rounds.each do |round|
   	  @byes[round.id] = Array.new
 	  end
-	  
+
 	  teams.each do |team|
 	    round_ids = team.games.collect{|g| g.round.id}
 	    @rounds.each do |round|
@@ -35,25 +35,49 @@ class StatisticsController < ApplicationController
       end
     end
   end
-  
+
   def team
   	begin
-  		@team = Team.find(params[:id], :include => [:games, {:players => :player_games}])
+  		@team = Team.find(params[:id], :include => [{:games => [:room, {:team_games => :team}]}, {:players => :player_games}])
   	rescue ActiveRecord::RecordNotFound
-  		
+  		flash[:error] = "Team was not found."
+                redirect_to :action => 'standings'
   	end
+
+  	@types = QuestionType.find(:all, :order => 'value desc')
   end
-  
+
   def personal
-  
+    @players_all = Player.find(:all, :include => [:team, :player_games]).sort{|a,b| b.pp20 <=> a.pp20}
+    @players_tuh_cut = @players_all.select{|p| $tournament.tuh_cutoff.nil? or p.tuh >= $tournament.tuh_cutoff}
+    @players_neg = @players_all.sort{|a,b| sort_negs(a,b)}[0,20]
+
+    @max_round = Round.find(:first, :order => 'number', :conditions => ["play_complete is null or play_complete != ?", true]).number
+    @types = QuestionType.find(:all, :order => 'value desc')
   end
-  
+
   def sort_teams(a,b,bracket)
   	if a.wins(bracket) == b.wins(bracket)
   		b.pp20(bracket) <=> a.pp20(bracket)
   	else
   		b.wins(bracket) <=> a.wins(bracket)
   	end
+  end
+
+  def sort_players(a,b)
+    if a.pp20 == b.pp20
+      b.points <=> a.points
+    else
+      b.pp20 <=> a.pp20
+    end
+  end
+
+  def sort_negs(a,b)
+    if a.tu_neg == b.tu_neg
+      b.neg20 <=> a.neg20
+    else
+      b.tu_neg <=> a.tu_neg
+    end
   end
 
 end
--- app/models/team_game.rb
+++ app/models/team_game.rb
@@ -2,16 +2,24 @@ class TeamGame < ActiveRecord::Base
   belongs_to :team
   belongs_to :game
   has_many :player_games, :dependent => :destroy
-  
+
   validates_presence_of :game
   validates_numericality_of :points, :only_integer => true, :allow_nil => true
   validates_numericality_of :tossups_correct, :only_integer => true, :allow_nil => true
   validates_numericality_of :tossup_points, :only_integer => true, :allow_nil => true
   validates_numericality_of :bonus_points, :only_integer => true, :allow_nil => true
   validate :pts_mod_5
-  
+
   def pts_mod_5
-  	errors.add_to_base("Points must be 0 mod 5") unless (points.nil? || points % 5 == 0)
+    errors.add_to_base("Points must be 0 mod 5") unless (points.nil? || points % 5 == 0)
   end
-  
+
+  def other_team
+    game.team_games[ordering % 2].team
+  end
+
+  def other_tg
+    game.team_games[ordering % 2]
+  end
+
 end
