e6b3d8dd710888891f787b602faa7210ea698b9f
Jim Puls <jim@nondifferentiable.com>
2009-04-03 11:08:49 -0700

    Rails 2.3.2.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -27,6 +27,9 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  
+  # config.gem "twitter"
+  # config.time_zone = 'UTC'
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
